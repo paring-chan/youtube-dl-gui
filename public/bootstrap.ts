84835a63630ada1d0067353b99f44ed7fe55573b
파랑이 <parangee9706@gmail.com>
2021-04-29 21:39:16 +0900

    feat: re-initialize

---
+++ public/bootstrap.ts
@@ -0,0 +1,37 @@
+import {app, BrowserWindow} from 'electron'
+import * as isDev from 'electron-is-dev'
+import * as path from 'path'
+
+let mainWindow: BrowserWindow | undefined
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        center: true,
+        kiosk: !isDev,
+        resizable: false,
+        fullscreenable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            devTools: isDev
+        }
+    })
+
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+
+    mainWindow.on('closed', () => (mainWindow = undefined))
+
+    mainWindow.focus()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+    if (!mainWindow) createWindow()
+})
