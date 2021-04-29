912bac4c241739d943ab125a127db840051b19b2
파랑이 <parangee9706@gmail.com>
2021-04-29 22:43:31 +0900

    feat: use RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { CssBaseline } from '@material-ui/core'
 import Header from './components/Header'
+import { RecoilRoot } from 'recoil'
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <CssBaseline />
       <Header />
       app
-    </>
+    </RecoilRoot>
   )
 }
 
