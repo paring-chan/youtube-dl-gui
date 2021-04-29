9325a40dd9dae4f302a328ef4e713b7dea1ae8d7
파랑이 <parangee9706@gmail.com>
2021-04-29 22:35:17 +0900

    feat: add button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import { Toolbar } from '@material-ui/core'
+import { IconButton, Toolbar, Tooltip, Typography } from '@material-ui/core'
+import { Add } from '@material-ui/icons'
 
 const Header = () => {
-  return <Toolbar>sans</Toolbar>
+  return (
+    <Toolbar>
+      <Typography variant='h6'>Youtube-DL</Typography>
+      <div style={{ flexGrow: 1 }} />
+      <Tooltip title='추가하기'>
+        <IconButton>
+          <Add />
+        </IconButton>
+      </Tooltip>
+    </Toolbar>
+  )
 }
 
 export default Header
