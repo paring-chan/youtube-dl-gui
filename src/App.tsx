0f9ea6efa0124fb3479611330925d8a612e31452
파랑이 <parangee9706@gmail.com>
2021-04-29 22:12:11 +0900

    feat: format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import {HashRouter, Route} from "react-router-dom";
-import Landing from "./views/Landing";
+import React from 'react'
+import { HashRouter, Route } from 'react-router-dom'
+import Landing from './views/Landing'
 
 function App() {
   return (
     <>
-      {
-        global.process.platform === 'darwin' && (
-            <>
-              {/* @ts-ignore */}
-              <div style={{width: '100vw', height: 35, WebkitAppRegion: 'drag', position: 'fixed'}} />
-            </>
-        )
-      }
-      <div style={{
-          padding: 40
-      }}>
-          <HashRouter>
-              <Route exact path="/" component={Landing}/>
-          </HashRouter>
+      {global.process.platform === 'darwin' && (
+        <>
+          {/* @ts-ignore */}
+          <div
+            style={{
+              width: '100vw',
+              height: 35,
+              WebkitAppRegion: 'drag',
+              position: 'fixed',
+            }}
+          />
+        </>
+      )}
+      <div
+        style={{
+          padding: 40,
+        }}
+      >
+        <HashRouter>
+          <Route exact path='/' component={Landing} />
+        </HashRouter>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Landing = () => {
-    return (
-        <div>
-            대충 홈임
-            </div>
-    );
-};
+  return <div>대충 홈임</div>
+}
 
-export default Landing;
+export default Landing
