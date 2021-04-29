1a7a6f21a124f87860a70b05f212e5ae97f41776
파랑이 <parangee9706@gmail.com>
2021-04-29 22:19:34 +0900

    many changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
-import Landing from './views/Landing'
 
 function App() {
   return (
     <>
-      {global.process.platform === 'darwin' && (
-        <>
-          <div
-            style={{
-              width: '100vw',
-              height: 35,
-              /* @ts-ignore */
-              WebkitAppRegion: 'drag',
-              position: 'fixed',
-            }}
-          />
-        </>
-      )}
-      <div
-        style={{
-          padding: 40,
-        }}
-      >
-        <HashRouter>
-          <Route exact path='/' component={Landing} />
-        </HashRouter>
-      </div>
+        app
     </>
   )
 }
--- src/views/Landing.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Landing = () => {
-  return <div>대충 홈임</div>
-}
-
-export default Landing
