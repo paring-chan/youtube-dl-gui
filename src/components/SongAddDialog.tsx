992efedf423e9308764c515d2acd4332652cc80c
파랑이 <parangee9706@gmail.com>
2021-04-30 15:38:53 +0900

    feat: dialog title

--- src/components/SongAddDialog.tsx
+++ src/components/SongAddDialog.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import { addDialogOpen } from '../store'
-import { Dialog } from '@material-ui/core'
+import { Dialog, DialogTitle } from '@material-ui/core'
 
 const SongAddDialog = () => {
   const [open, setOpen] = useRecoilState(addDialogOpen)
 
   return (
     <Dialog open={open} onClose={() => setOpen(false)}>
-      ㅁㄴㅇㄹ
+      <DialogTitle>영상 추가하기</DialogTitle>
     </Dialog>
   )
 }
