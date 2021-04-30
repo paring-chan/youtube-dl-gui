1053696f6d761ec52506a441aeab599c01c40836
파랑이 <parangee9706@gmail.com>
2021-04-30 14:49:21 +0900

    feat: song add dialog

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { CssBaseline } from '@material-ui/core'
 import Header from './components/Header'
 import { RecoilRoot } from 'recoil'
+import SongAddDialog from './components/SongAddDialog'
 
 function App() {
   return (
     <RecoilRoot>
       <CssBaseline />
       <Header />
+      <SongAddDialog />
       app
     </RecoilRoot>
   )
---
+++ src/components/SongAddDialog.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SongAddDialog = () => {
+  return <div></div>
+}
+
+export default SongAddDialog
