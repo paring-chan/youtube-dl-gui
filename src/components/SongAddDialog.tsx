591420b488e2ac2a6469beebe62a22814e2cde99
파랑이 <parangee9706@gmail.com>
2021-04-30 15:49:27 +0900

    feat: add dialog close button

--- src/components/SongAddDialog.tsx
+++ src/components/SongAddDialog.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import { addDialogOpen } from '../store'
-import { Dialog, DialogTitle } from '@material-ui/core'
+import {
+  Dialog,
+  DialogContent,
+  IconButton,
+  TextField,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import { Close } from '@material-ui/icons'
 
 const SongAddDialog = () => {
   const [open, setOpen] = useRecoilState(addDialogOpen)
 
   return (
-    <Dialog open={open} onClose={() => setOpen(false)}>
-      <DialogTitle>영상 추가하기</DialogTitle>
+    <Dialog
+      open={open}
+      disableBackdropClick
+      onClose={() => setOpen(false)}
+      fullWidth
+    >
+      <Toolbar>
+        <Typography variant='h6'>영상 추가하기</Typography>
+        <div style={{ flexGrow: 1 }} />
+        <IconButton onClick={() => setOpen(false)}>
+          <Close />
+        </IconButton>
+      </Toolbar>
+      <DialogContent>
+        <TextField fullWidth placeholder='곡 제목 또는 URL을 입력해주세요' />
+      </DialogContent>
     </Dialog>
   )
 }
