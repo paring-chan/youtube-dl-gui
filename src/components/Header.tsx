58eebad7d3fa673cf34e247c9325faca2b271952
파랑이 <parangee9706@gmail.com>
2021-04-30 15:18:37 +0900

    feat: implement song add dialog open and close

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { IconButton, Toolbar, Tooltip, Typography } from '@material-ui/core'
 import { Add } from '@material-ui/icons'
+import { useSetRecoilState } from 'recoil'
+import { addDialogOpen } from '../store'
 
 const Header = () => {
+  const setDialogOpen = useSetRecoilState(addDialogOpen)
+
   return (
     <Toolbar>
       <Typography variant='h6'>Youtube-DL</Typography>
       <div style={{ flexGrow: 1 }} />
       <Tooltip title='추가하기'>
-        <IconButton>
+        <IconButton onClick={() => setDialogOpen(true)}>
           <Add />
         </IconButton>
       </Tooltip>
--- src/components/SongAddDialog.tsx
+++ src/components/SongAddDialog.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { addDialogOpen } from '../store'
+import { Dialog } from '@material-ui/core'
 
 const SongAddDialog = () => {
-  return <div></div>
+  const [open, setOpen] = useRecoilState(addDialogOpen)
+
+  return (
+    <Dialog open={open} onClose={() => setOpen(false)}>
+      ㅁㄴㅇㄹ
+    </Dialog>
+  )
 }
 
 export default SongAddDialog
