f2f7e1623bd923a3fd82efa5f37def7dea6176ab
파랑이 <parangee9706@gmail.com>
2021-05-01 10:47:42 +0900

    feat: implement no tracks message

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { tracksState } from '../store'
+import { List, Typography } from '@material-ui/core'
 
 const TrackList = () => {
-  return <div>list</div>
+  const [tracks, setTracks] = useRecoilState(tracksState)
+  return tracks.length ? (
+    <List></List>
+  ) : (
+    <Typography variant='body1' color='textSecondary'>
+      추가된 영상이 없습니다.
+    </Typography>
+  )
 }
 
 export default TrackList
