a5c5b2f2b5c836ed50d1965a18fc2f71c0b1f9d4
파랑이 <parangee9706@gmail.com>
2021-05-01 12:40:49 +0900

    feat: track list

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import { tracksState } from '../store'
-import { List, Typography } from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  Typography,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Card,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
 
 const TrackList = () => {
   const [tracks, setTracks] = useRecoilState(tracksState)
   return tracks.length ? (
-    <List></List>
+    <Card variant='outlined'>
+      <List>
+        {tracks.map((x, i) => (
+          <ListItem key={i}>
+            <ListItemAvatar>
+              <Avatar src={x.thumbnail} />
+            </ListItemAvatar>
+            <ListItemText primary={x.title} secondary={x.author} />
+            <ListItemSecondaryAction>
+              <IconButton
+                onClick={() => {
+                  const patched = tracks.filter((y) => y.id !== x.id)
+                  setTracks(patched)
+                }}
+              >
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+    </Card>
   ) : (
     <Typography variant='body1' color='textSecondary'>
       추가된 영상이 없습니다.
