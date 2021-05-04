ac66377f3b368a1fff0f81d6d6c796f5f34175a9
파랑이 <parangee9706@gmail.com>
2021-05-04 13:12:16 +0900

    feat: add all button

--- src/components/SongAddDialog.tsx
+++ src/components/SongAddDialog.tsx
@@ -167,38 +167,42 @@ const SongAddDialog = () => {
                     {res.author.name}
                   </Typography>
                 </div>
-                {/*<div>*/}
-                {/*  /!*<Button*!/*/}
-                {/*  /!*  variant='outlined'*!/*/}
-                {/*  /!*  color='primary'*!/*/}
-                {/*  /!*  onClick={() => {*!/*/}
-                {/*  /!*    let patched = [...tracks]*!/*/}
-                {/*  /!*    patched.push(*!/*/}
-                {/*  /!*      ...res.items*!/*/}
-                {/*  /!*        .filter((x) => !patched.find((y) => x.id === y.id))*!/*/}
-                {/*  /!*        .map((x) => ({*!/*/}
-                {/*  /!*          id: x.id,*!/*/}
-                {/*  /!*          title: x.title,*!/*/}
-                {/*  /!*          thumbnail: x.bestThumbnail.url,*!/*/}
-                {/*  /!*          author: x.author.name,*!/*/}
-                {/*  /!*        }))*!/*/}
-                {/*  /!*    )*!/*/}
-                {/*  /!*    setTracks(patched)*!/*/}
-                {/*  /!*    enqueueSnackbar(*!/*/}
-                {/*  /!*      `영상 ${res.items.length}개가 추가되었습니다.`,*!/*/}
-                {/*  /!*      {*!/*/}
-                {/*  /!*        variant: 'success',*!/*/}
-                {/*  /!*      }*!/*/}
-                {/*  /!*    )*!/*/}
-                {/*  /!*    setOpen(false)*!/*/}
-                {/*  /!*  }}*!/*/}
-                {/*  /!*>*!/*/}
-                {/*  /!*  영상 {res.items.length}개 추가하기*!/*/}
-                {/*  /!*</Button>*!/*/}
-                {/*</div>*/}
               </CardContent>
               <Divider />
-
+              <div
+                style={{
+                  padding: 4,
+                }}
+              >
+                <Button
+                  variant='outlined'
+                  color='primary'
+                  fullWidth
+                  onClick={() => {
+                    let patched = [...tracks]
+                    patched.push(
+                      ...res.items
+                        .filter((x) => !patched.find((y) => x.id === y.id))
+                        .map((x) => ({
+                          id: x.id,
+                          title: x.title,
+                          thumbnail: x.bestThumbnail.url,
+                          author: x.author.name,
+                        }))
+                    )
+                    setTracks(patched)
+                    enqueueSnackbar(
+                      `영상 ${res.items.length}개가 추가되었습니다.`,
+                      {
+                        variant: 'success',
+                      }
+                    )
+                    setOpen(false)
+                  }}
+                >
+                  영상 {res.items.length}개 추가하기
+                </Button>
+              </div>
               <MUIList>
                 {res.items.map((x, y) => (
                   <ListItem key={y}>
